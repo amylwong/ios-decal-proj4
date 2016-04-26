e1ca9a35125849c26697728ad3c21a1e068d0359
Amy Wong <amylwong@berkeley.edu>
2016-04-26 02:12:46 -0700

    set up tabviews

---
+++ Curiocity/Curiocity/DestinationsViewController.swift
@@ -0,0 +1,25 @@
+//
+//  DestinationsViewController.swift
+//  Curiocity
+//
+//  Created by Amy Wong on 4/26/16.
+//  Copyright © 2016 Amy Wong. All rights reserved.
+//
+
+import UIKit
+
+class DestinationsViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
+
---
+++ Curiocity/Curiocity/MapViewController.swift
@@ -0,0 +1,25 @@
+//
+//  MapViewController.swift
+//  Curiocity
+//
+//  Created by Amy Wong on 4/26/16.
+//  Copyright © 2016 Amy Wong. All rights reserved.
+//
+
+import UIKit
+
+class MapViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
+
